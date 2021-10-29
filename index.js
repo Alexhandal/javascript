6b5361f64ec84a0a1ef7b0ee3db037816b150355
Alexhandal <alexanderhandal12@gmail.com>
2021-10-28 20:42:20 -0400

    imdone

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-const BMIcalculator = function (being) {
-  let BMI = being.weight / being.height ** 2;
+const calculateBMI = function (being) {
+  let BMI = Math.round((being.weight / being.height **2) * 10000);
   return BMI;
 };
 
 const Handal = {
   name: "Handal",
   weight: 60,
-  height: 1.8,
+  height: 172,
 };
 
-const BMI = function (being) {
+const showBMI = function (being) {
   let BMI = calculateBMI(being);
-  if (BMI < 18.5) {
-    console.log("you are underweight");
-  } else if (BMI > 18.5 && BMI < 25) {
-    console.log("you are healthy");
-  } else if (BMI > 25) {
-    console.log("You are fat");
+  if (BMI < 18.5){
+    console.log("U R Underweight");
+    alert("U R Underweight");
+  } else if (BMI > 18.5 && BMI <=25) {
+console.log("Why r u so healthy");
+alert("Why r u so healthy");
+  } else if (BMI>25) {
+    console.log("you are fattt");
+    alert("you are fattt");
   }
-};
+}
 
-CalculateBMI(Handal);
-console.log(BMI(Handal));
+console.log("Ur BMI");
+console.log(calculateBMI(Handal));
+showBMI(Handal);
